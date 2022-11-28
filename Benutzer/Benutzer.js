3eb7fe224aaf53c740c075ce1fe0c25443a79f57
Philipp <kommt noch.de>
2022-11-28 16:15:05 +0100

    Erste Synchro

---
+++ Benutzer/Benutzer.js
@@ -0,0 +1,14 @@
+
+class Benutzer {
+    constructor(id ,name, vorname, probezeit, schwerbehindert, gesamturlaub, resturlaub, beantragteTage ){
+        this.id = id;
+        this.name = name;
+        this.vorname = vorname;
+        this.probezeit = probezeit;
+        this.schwerbehindert = schwerbehindert;
+        this.gesamturlaub = gesamturlaub;
+        this.resturlaub = resturlaub;
+        this.beantragteTage = beantragteTage;
+    }
+
+}
---
+++ Berrechnungen/Urlaubstage_Berrechnung.js
@@ -0,0 +1,16 @@
+/*
+Deklaration und Inisialiserung der Variablen
+*/
+
+
+
+var urlaubstage = 30;
+var gestzUrlaub = 24;
+var betWoche = 5;
+var gestzWoche = 6;
+var ergebnis;
+
+ergebnis = (gestzUrlaub / gestzWoche) * betWoche;
+
+console.log(ergebnis);
+
