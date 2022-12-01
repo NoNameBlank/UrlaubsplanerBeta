f6b682ca1a4171eec07b516b1c36809925c1c3f7
PhiL <kommt noch.de>
2022-12-01 16:20:15 +0100

    Diagramme hinzugefügt

--- Benutzer/Benutzer.js
+++ Benutzer/Benutzer.js
@@ -1,6 +1,6 @@
 
 class Benutzer {
-    constructor(id ,name, vorname, probezeit, schwerbehindert, gesamturlaub, resturlaub, beantragteTage ){
+    constructor(id ,name, vorname, probezeit, schwerbehindert, gesamturlaub, resturlaub, beantragteTage, admin ){
         this.id = id;
         this.name = name;
         this.vorname = vorname;
@@ -9,6 +9,7 @@ class Benutzer {
         this.gesamturlaub = gesamturlaub;
         this.resturlaub = resturlaub;
         this.beantragteTage = beantragteTage;
+        this.admin = admin;
     }
 
 }
